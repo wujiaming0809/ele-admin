df23155b325395ea0b53eda49e760dc2ebac3352
moyunsuibuyao <839389144@qq.com>
2020-03-20 11:02:35 +0800

    npm命令行创建文件

---
+++ generate/api.js
@@ -0,0 +1,7 @@
+/**
+ * Created by wujiaming on 2020/3/20
+ **/
+
+module.exports = {
+  apiTemplate: () => '',
+};
--- generate/index.js
+++ generate/index.js
@@ -12,7 +12,9 @@ const successLog = message => console.log(chalk.blue(`${message}`));
 const errorLog = error => console.log(chalk.red(`${error}`));
 
 // 导入模板
-const { vueTemplate, entryTemplate } = require('./template');
+const { vueTemplate } = require('./template');
+const { apiTemplate } = require('./api');
+const { routerTemplate } = require('./router');
 
 // 生成文件
 const generateFile = (path, data) => {
@@ -32,24 +34,52 @@ const generateFile = (path, data) => {
   });
 };
 
+// 递归创建目录
+const mkDirs = (directory, callback) => {
+  const exists = fs.existsSync(directory);
+  if (exists) {
+    callback();
+  } else {
+    mkDirs(path.dirname(directory), () => {
+      fs.mkdirSync(directory);
+      callback();
+    });
+  }
+};
+
+const dotExistDirectoryCreate = directory => new Promise((resolve) => {
+  mkDirs(directory, () => {
+    resolve(true);
+  });
+});
+
 log('请输入要生成的页面组件的名称，会放在 view/目录下');
 let componentName = '';
 process.stdin.on('data', async (chunk) => {
   // 组件名称
   const inputName = String(chunk).trim().toString();
-  // Vue页面组件路径
-  const componentPath = resolve('../../src/views', inputName);
-  // vue文件
+  // 创建路径（文件夹）
+  const componentPath = resolve('../src/views', inputName);
+  const apiPath = resolve('../src/api', inputName);
+  // 创建文件
   const vueFile = resolve(componentPath, 'index.vue');
-  // 入口文件
-  const entryFile = resolve(componentPath, 'entry.js');
-  // 判断组件文件夹是否存在
+  const apiFile = resolve(apiPath, `${inputName}.js`);
+  const routerFile = resolve('../src/router', `${inputName}.js`);
+  // 判断文件夹是否存在
   const hasComponentExists = fs.existsSync(componentPath);
+  const hasApiExists = fs.existsSync(apiPath);
+  const hasRouterExists = fs.existsSync(routerFile);
   if (hasComponentExists) {
-    errorLog(`${inputName}页面组件已存在，请重新输入`);
+    errorLog(`${inputName} - vue文件已存在，请重新输入`);
+  } else if (hasApiExists) {
+    errorLog(`${inputName} - api文件已存在，请重新输入`);
+  } else if (hasRouterExists) {
+    errorLog(`${inputName} - router文件已存在，请重新输入`);
   } else {
-    log(`正在生成component目录${componentPath}`);
+    log(`正在生成组件目录${componentPath}`);
+    log(`正在生成api目录${apiPath}`);
     await dotExistDirectoryCreate(componentPath);
+    await dotExistDirectoryCreate(apiPath);
   }
   try {
     // 获取组件名
@@ -61,8 +91,10 @@ process.stdin.on('data', async (chunk) => {
     }
     log(`正在生成vue文件${vueFile}`);
     await generateFile(vueFile, vueTemplate(componentName));
-    log(`正在生成 entry 文件 ${entryFile}`);
-    await generateFile(entryFile, entryTemplate(componentName));
+    log(`正在生成 api 文件 ${apiFile}`);
+    await generateFile(apiFile, apiTemplate(componentName));
+    log(`正在生成 路由 文件 ${routerFile}`);
+    await generateFile(routerFile, routerTemplate(componentName));
     successLog('生成成功');
   } catch (e) {
     errorLog(e.message);
@@ -73,21 +105,3 @@ process.stdin.on('end', () => {
   log('exit');
   process.exit();
 });
-
-const dotExistDirectoryCreate = directory => new Promise((resolve) => {
-  mkdirs(directory, () => {
-    resolve(true);
-  });
-});
-// 递归创建目录
-const mkdirs = (directory, callback) => {
-  const exists = fs.existsSync(directory);
-  if (exists) {
-    callback();
-  } else {
-    mkdirs(path.dirname(directory), () => {
-      fs.mkdirSync(directory);
-      callback();
-    });
-  }
-};
